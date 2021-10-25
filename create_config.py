f8f62276a332b6487b4483acdb2e510a0c90be76
Ratnadeep Bhattacharya <ratnadeep.bhattacharya1983@gmail.com>
2021-10-24 18:44:41 -0600

    make graph creation robust

--- create_config.py
+++ create_config.py
@@ -1,13 +1,37 @@
 #!/usr/bin/env python3
 import networkx as nx
 import yaml
+import sys
+from numpy.random import randint
 
+if len(sys.argv) < 3:
+    sys.stderr.write("not enough arguments\n")
+    sys.stderr.write(
+        f"Usage: {__file__} <node_count|int> <edge_count|int> <max_cost|int>\n")
+    sys.exit(1)
+
+_node_count = int(sys.argv[1])
+_edge_count = int(sys.argv[2])
+max_cost = int(sys.argv[3])
 # values to be collected from users
 node_count = 3
 edge_count = 2
+# ultimately, we would want every service to start a single pod
+_replicas = [1] * _node_count
+print(_replicas)  # debug
+
 replicas = [2, 4, 2]
+
 costs = [2, 2, 4]
+# create a list of random costs
+_costs = randint(max_cost + 1, size=_node_count)
+print(_costs)  # debug
+
 bads = [0, 0, 1]
+# randomly allow some services to have bad pods
+_bads = randint(2, size=_node_count)
+print(_bads)  # debug
+
 fname = "test-config.yaml"
 # =================================
 
@@ -29,9 +53,8 @@ for item in map.items():
     config_tmpl['cost'] = costs[idx]
     config_tmpl['bads'] = bads[idx]
     svc = []
-    # too expensive?
+    # too expensive???
     for edge in H.edges:
-        # print(edge[0][-1])
         if edge[0] == 'testapp-svc-%s' % idx:
             svc.append('testapp-svc-%s:5000/svc/%s' %
                        (edge[1][-1], edge[1][-1]))
@@ -40,8 +63,6 @@ for item in map.items():
                        (edge[0][-1], edge[0][-1]))
     config_tmpl['svc'] = svc
     configs.append(config_tmpl)
-
-# print(configs)
 
 with open(fname, 'w') as file:
     yaml.dump(configs, file)
