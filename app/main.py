d92a7c9a5eede32c80011ed087294be12617212c
Ratnadeep Bhattacharya <ratnadeep.bhattacharya1983@gmail.com>
2021-10-27 15:21:41 -0600

    merge

--- app/main.py
+++ app/main.py
@@ -29,15 +29,17 @@ FREQ = 0
 
 app = Flask(__name__)
 
+
 def isPrime(x) -> bool:
     k = 0
     for i in range(2, x // 2 + 1):
-        if(x % i==0):
+        if(x % i == 0):
             k = k + 1
     if k <= 0:
         return True
     else:
         return False
+
 
 def largestPrime(x) -> int:
     prime = -1
@@ -46,19 +48,28 @@ def largestPrime(x) -> int:
             prime = num
     return prime
 
+
 def parse_config() -> list:
     """ Parse the config file """
     with open("config.yaml", 'r') as y:
         read_data = yaml.load(y, Loader=yaml.FullLoader)
     return read_data
 
+
 def cpu_usage() -> float:
     """ Get CPU usage """
     return psutil.cpu_percent(interval=0.5)
 
+
 def mem_usage():
     """ Get memory usage """
     return psutil.virtual_memory().percent
+
+
+@app.route("/health_check", methods=["GET"])
+def health_check():
+    return "OK\n"
+
 
 @app.route("/statistics", methods=['GET'])
 def get_stats() -> dict:
@@ -68,11 +79,13 @@ def get_stats() -> dict:
             'local_response_time':  LOCAL_RESPONSE_TIME,
             'total_response_time': TOTAL_RESPONSE_TIME,
             'perf_counter': FREQ
-    }
+            }
+
 
 def failure_response(url: str, status: int) -> Response:
     """ Send failure response """
     return Response('Error: failed to access {}\n'.format(url), status=status)
+
 
 @app.route('/svc/<int:index>', methods=['GET'])
 def serve(index) -> dict:
@@ -86,43 +99,46 @@ def serve(index) -> dict:
     tmp = time.perf_counter()
     FREQ = tmp - START_TIME
     START_TIME = tmp
-    
+
     start = time.perf_counter()
 
     logger = logging.getLogger("mico_serve_logger")
 
-    index = list({index})[0] # get the number from the param
-    data = parse_config() # get config data
-    if len(data) < index + 1: # number of elements in the config should equal to or more than the index
+    index = list({index})[0]  # get the number from the param
+    data = parse_config()  # get config data
+    if len(data) < index + 1:  # number of elements in the config should equal to or more than the index
         sys.stderr.write("Error: Config file does not contain correct index")
         return failure_response("svc-{} doesn't exist".format(index), 500)
 
-    d = data[index] # get the config for the given index
-    urls = d['svc'] # get all urls to be called
-    cost = d['cost'] # cost of this call
+    d = data[index]  # get the config for the given index
+    urls = d['svc']  # get all urls to be called
+    cost = d['cost']  # cost of this call
 
     p = 1_000
     for i in range(cost):
         largestPrime(p)
     LOCAL_RESPONSE_TIME = time.time() - start
-    
+
     if urls is None or len(urls) == 0:  # url list is empty => this is a leaf node
         TOTAL_RESPONSE_TIME = time.perf_counter() - start
-        return {'urls': None, 'cost': cost }
-    else: # non-leaf node
-        try: # request might fail
-            _ = joblib.Parallel(prefer="threads", n_jobs=len(urls))((delayed(requests.get)("http://{}".format(url)) for url in urls))
-        except ConnectionError as e: # send page not found if it does
+        return {'urls': None, 'cost': cost}
+    else:  # non-leaf node
+        try:  # request might fail
+            _ = joblib.Parallel(prefer="threads", n_jobs=len(urls))(
+                (delayed(requests.get)("http://{}".format(url)) for url in urls))
+        except ConnectionError as e:  # send page not found if it does
             s = e.args[0].args[0].split()
             host = s[0].split('=')[1].split(',')[0]
             port = s[1].split('=')[1].split(')')[0]
 
             TOTAL_RESPONSE_TIME = time.perf_counter() - start
-            
+
             return failure_response("{}:{}".format(host, port), 404)
-        
+
         TOTAL_RESPONSE_TIME = time.perf_counter() - start
-        return {'urls': list(urls), 'cost': cost} # doesn't matter what is returned
+        # doesn't matter what is returned
+        return {'urls': list(urls), 'cost': cost}
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
