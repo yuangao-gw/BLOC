a7ad021e95a8e7bfd12f2996ff707994204871a6
Ratnadeep Bhattacharya <ratnadeep.bhattacharya1983@gmail.com>
2022-03-01 17:09:50 -0600

    scripts

--- scripts/collect_load_dist.sh
+++ scripts/collect_load_dist.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-while true; do for nm in $(kubectl get pods | grep svc-2  | awk '{print $1}'); do kubectl logs $nm micoproxy | grep incoming | wc -l | tr '\n' ','; done; echo; sleep 2; done | tee ../logs/dist.log
+while true; do for nm in $(kubectl get pods | grep svc-2  | awk '{print $1}'); do kubectl logs $nm micoproxy | grep incoming | wc -l | tr '\n' ','; done; echo; sleep 2; done | tee ./logs/dist.log
--- scripts/collect_svc_tms.sh
+++ scripts/collect_svc_tms.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+# Collect from Service 0
+rm -rf ./logs/svc_0_times.log
+for pod in `kubectl get pods | grep svc-0 | awk '{print $1}'`; do echo $pod >> ./logs/svc_0_times.log; kubectl logs $pod testapp-svc-0 | grep INFO | grep Success | awk '{print $7,$9}' >> ./logs/svc_0_times.log; done
 # Collect from Service 1
-rm -rf svc_1_times.log
-for pod in `kubectl get pods | grep svc-1 | awk '{print $1}'`; do echo $pod >> svc_1_times.log; kubectl logs $pod testapp-svc-1 | grep INFO | grep Success | awk '{print $7$9}' >> svc_1_times.log; done
+rm -rf ./logs/svc_1_times.log
+for pod in `kubectl get pods | grep svc-1 | awk '{print $1}'`; do echo $pod >> ./logs/svc_1_times.log; kubectl logs $pod testapp-svc-1 | grep INFO | grep Success | awk '{print $7,$9}' >> ./logs/svc_1_times.log; done
 # Collect from Service 2
-rm -rf svc_2_times.log
-for pod in `kubectl get pods | grep svc-2 | awk '{print $1}'`; do echo $pod >> svc_2_times.log; kubectl logs $pod testapp-svc-2 | grep INFO | grep "No URLs" | awk '{print $7$9}' >> svc_2_times.log; done
+rm -rf ./logs/svc_2_times.log
+for pod in `kubectl get pods | grep svc-2 | awk '{print $1}'`; do echo $pod >> ./logs/svc_2_times.log; kubectl logs $pod testapp-svc-2 | grep INFO | grep "No URLs" | awk '{print $8,$10}' >> ./logs/svc_2_times.log; done
